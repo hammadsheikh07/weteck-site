ef37871bf485b741c7afbfca34fcf7ae33f24ee3
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-08-12 22:34:03 +0500

    error

--- components/Analytics/Analytics.js
+++ components/Analytics/Analytics.js
@@ -3,8 +3,8 @@ import React from "react";
 const Analytics = () => {
   return (
     <div className="text-textcolor font-textfont mt-24 text-center">
-      <h1 class="text-body1 text-center">Your trust gives us strength</h1>
-      <h1 class="text-head1 font-bold text-center">To Give Out Our Best</h1>
+      <h1 className="text-body1 text-center">Your trust gives us strength</h1>
+      <h1 className="text-head1 font-bold text-center">To Give Out Our Best</h1>
       <div className="flex">
         <div className="mt-10 mx-auto flex justify-center items-center bg-gradient-to-b from-conclr1 to-conclr2 rounded-full space-x-24 py-10 px-52">
           <div>
