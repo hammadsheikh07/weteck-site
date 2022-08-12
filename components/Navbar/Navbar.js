cd9f2ee6f33cb1a447b659a6885f7ad888e48585
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-08-12 19:46:41 +0500

    navbar component

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-14 mt-4">
+      <div>
+        <img src="/wetecklogo.png" width={"150px"} />
+      </div>
+      <div>
+        <ul className="text-textcolor flex justify-between font-textfont divide-x divide-solid divide-textcolor">
+         <a href=""> <li className="px-3">Our Services</li></a>
+         <a href=""> <li className="px-3">Portfolio</li></a>
+         <a href=""> <li className="px-3">About us</li></a>
+        </ul>
+      </div>
+      <div>
+        <button className="background-transparent border-2 border-solid border-textcolor rounded-full p-3 text-textcolor font-textfont w-36">
+          Contact Us
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
