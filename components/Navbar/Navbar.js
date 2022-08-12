fc14e10d3a36c34d044abc024a33178cab5cd56c
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-08-12 22:31:35 +0500

    navbar lines updated

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,13 +4,24 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-14 mt-4">
       <div>
-        <Image src="/WeteckLogo.png" width={"150px"} height={"50px"}/>
+        <Image src="/WeteckLogo.png" width={"150px"} height={"50px"} />
       </div>
       <div>
-        <ul className="text-textcolor flex justify-between font-textfont divide-x divide-solid divide-textcolor">
-         <a href=""> <li className="px-3">Our Services</li></a>
-         <a href=""> <li className="px-3">Portfolio</li></a>
-         <a href=""> <li className="px-3">About us</li></a>
+        <ul className="text-textcolor flex justify-between font-textfont items-center  space-x-4">
+          <a href="">
+            {" "}
+            <li className="">Our Services</li>
+          </a>
+          <hr className="border border-solid border-textcolor h-6"></hr>
+          <a href="">
+            {" "}
+            <li className="">Portfolio</li>
+          </a>
+          <hr className="border border-solid border-textcolor h-6"></hr>
+          <a href="">
+            {" "}
+            <li className="">About us</li>
+          </a>
         </ul>
       </div>
       <div>
